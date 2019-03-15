2aa6ed53eaf4673898be295493537218388dca53
Valentina Adami <valeadami@gmail.com>
2019-03-15 10:59:34 +0100

    ora ok userid

--- indexnew.js
+++ indexnew.js
@@ -506,7 +506,7 @@ function callAVANEW(agent) {
           break;
         //28/01/2019
         case 'getNumeroMatricola':
-          controller.getCarriera('s262501').then((carriera)=> {
+          controller.getCarriera('s262502').then((carriera)=> {
             var strTemp='';
             strTemp+='' + carriera.matricola;
           console.log('chiedo il numero di matricola ...');
